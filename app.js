fd0cd94a60fb3036198351f5b0952137d1fe6aed
YU Jing <yuki5834444@gmail.com>
2021-02-06 18:25:42 +0800

    basic implementation

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const PORT = 3000;
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat-msg', (message) => {
+    io.emit('chat-msg', message);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log('listening on:', PORT);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,13 +42,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// mongoose connection setup
+var mongoose = require('mongoose');
+var uri = 'mongodb+srv://admin:admin@cluster0.puvzp.mongodb.net/lib_db?retryWrites=true&w=majority';
+mongoose.connect(uri, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
